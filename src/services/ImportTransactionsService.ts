7042d23a0aa6eb6adcdba85ee2a3ea242c556993
dnogueir <daniell_dng@hotmail.com>
2020-05-01 16:31:37 -0300

    finishing database import

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,9 +1,11 @@
 import path from 'path';
 import fs from 'fs';
+import { getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
 import LoadCsv from '../utils/CsvImportUitl';
 import HandleCategoryService from './HandleCategoryService';
 import CreateTransactionService from './CreateTransactionService';
+import Category from '../models/Category';
 
 class ImportTransactionsService {
   async execute(data: string): Promise<Transaction[]> {
@@ -11,27 +13,103 @@ class ImportTransactionsService {
     const csvImport = new LoadCsv();
     const transactions = await csvImport.execute(csvFilePath);
     const insertedTransactions: Transaction[] = [];
+    const transactionsToBeInserted: Transaction[] = [];
+    const insertedCategories: string[] = [];
+    const existingCategoriesTitle: string[] = [];
+    const categoryArray: Category[] = [];
+    const categoryRepository = getRepository(Category);
+    const transactionRepository = getRepository(Transaction);
 
-    const handleCategoryService = new HandleCategoryService();
-    const createTransactionService = new CreateTransactionService();
-
-    await transactions.forEach(async trans => {
-      console.log(trans);
-      const category_id = await handleCategoryService.execute(trans[3]);
-
-      const transaction = await createTransactionService.execute({
-        title: trans[0],
-        value: trans[2],
-        type: trans[1],
-        category_id,
-      });
-      insertedTransactions.push(transaction);
-      console.log('trans');
+    // getting categories
+    transactions.forEach(trans => {
+      insertedCategories.push(trans[3]);
     });
+
+    const nonRepeatedCategories = insertedCategories.filter(function (
+      elem,
+      index,
+      self,
+    ) {
+      return index === self.indexOf(elem);
+    });
+
+    const existingCategories = await categoryRepository
+      .createQueryBuilder('category')
+      .where('category.title IN (:...titles)', {
+        titles: nonRepeatedCategories,
+      })
+      .getMany();
+
+    if (
+      existingCategories &&
+      existingCategories.length < nonRepeatedCategories.length
+    ) {
+      existingCategories.forEach(item => {
+        existingCategoriesTitle.push(item.title);
+      });
+
+      const categoriesToBeInserted = nonRepeatedCategories.filter(category => {
+        return !existingCategoriesTitle.includes(category);
+      });
+
+      categoriesToBeInserted.forEach(category => {
+        const cat = new Category();
+        cat.title = category;
+        categoryArray.push(cat);
+      });
+
+      await categoryRepository
+        .createQueryBuilder()
+        .insert()
+        .into(Category)
+        .values(categoryArray)
+        .execute();
+    }
+
+    // getting categories ids
+    const categories = await categoryRepository
+      .createQueryBuilder('category')
+      .where('category.title IN (:...titles)', {
+        titles: nonRepeatedCategories,
+      })
+      .getMany();
+
+    transactions.forEach(item => {
+      const transaction = new Transaction();
+      transaction.title = item[0];
+      transaction.value = item[2];
+      transaction.type = item[1];
+      transaction.category = categories.find(
+        element => element.title === item[3],
+      )?.id;
+      transactionsToBeInserted.push(transaction);
+    });
+
+    console.log(transactionsToBeInserted);
+
+    const trans = await transactionRepository
+      .createQueryBuilder()
+      .insert()
+      .into(Transaction)
+      .values(transactionsToBeInserted)
+      .execute();
+
+    console.log(trans.identifiers);
+    const transIds: string[] = [];
+    trans.identifiers.forEach(item => {
+      transIds.push(item.id);
+    });
+
+    const trans2 = await transactionRepository
+      .createQueryBuilder('transaction')
+      .where('transaction.id IN (:...ids)', {
+        ids: transIds,
+      })
+      .getMany();
 
     fs.unlinkSync(csvFilePath);
 
-    return insertedTransactions;
+    return trans2;
   }
 }
 
